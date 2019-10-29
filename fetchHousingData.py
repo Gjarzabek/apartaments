a8dcb36527ad68e93250db772fee3dae6a24a756
Grzesiek <grzegorz.jjarz@gmail.com>
2019-10-30 00:10:54 +0100

    manipulacja danymi

--- fetchHousingData.py
+++ fetchHousingData.py
@@ -6,6 +6,13 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.preprocessing import Imputer, OneHotEncoder, LabelEncoder
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+#1 TODO: replace gaps in data by median
+#2 TODO: replace strings in dataframe by vectors ex. 000100
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
 HOUSING_PATH = os.path.join("zestawy danych", "mieszkania")
@@ -24,7 +31,9 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
 
 def load_housing_data(housing_path=HOUSING_PATH):
     csv_path = os.path.join(housing_path, "housing.csv")
-    return pd.read_csv(csv_path)
+    data = pd.read_csv(csv_path)
+    #imputer = Imputer(strategy="median") fill gaps in table
+    return data
 
 def load_train_data(housing_path=HOUSING_PATH):
     csv_path = os.path.join(housing_path, "train.csv")
@@ -54,9 +63,55 @@ def split_train_test(dataset, test_ratio):
     #train_indicies = shuffled_indices[test_set_size:]
     #train_data = data.iloc[train_indicies]
     #test_data = data.iloc[test_indicies]
+
     train_data.to_csv(os.path.join(HOUSING_PATH, "train.csv"), index=None, header=True)
     test_data.to_csv(os.path.join(HOUSING_PATH, "test.csv"), index=None, header=True)
     print("Train and Test data saved to .csv files")
+
+
+
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+    roomsId, bedroomsId, papulationId, householdId = 3, 4, 5, 6
+
+    def __init__(self, add_bedrooms_per_room = True):
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+
+    def fit(self, X, y = None):
+        return self
+
+    def transform(self, X, y = None):
+        rooms_per_family = X[:,self.roomsId] / X[:, self.householdId]
+        population_per_family = X[:, self.papulationId] / X[:, self.householdId]
+        if self.add_bedrooms_per_room:
+            bedrooms_per_room = X[:, self.bedroomsId] / X[:, self.roomsId]
+            return np.c_[X, rooms_per_family, population_per_family, bedrooms_per_room]
+        else:
+            return np.c_[X, rooms_per_family, population_per_family]
+
+num_pipeline = Pipeline([
+    ('imputer', Imputer(strategy="median")),
+    ('attribs_adder', CombinedAttributesAdder()),
+    ('std_scaler', StandardScaler())
+])
+
+def encoded_data_without_gaps():
+    data = load_train_data()
+    imputer = Imputer("median")
+    housing_num = data.drop("ocean_proximity", axis=1) # usuwamy kolumne ze stringami
+    #housing_num_tr = num_pipeline.fit_tranfrom(housing_num)
+    imputer.fill(housing_num)
+    X = imputer.transform(housing_num)
+    housing_wihout_gaps = pd.DataFrame(X, columns=housing_num.columns)
+    encoder = LabelEncoder()
+    housing_cat = data["ocean_proximity"]
+    housing_cat_encoded = encoder.fit_transform(housing_cat)
+
+    encoder = OneHotEncoder()
+    housing_cat_1hot = encoder.fit_transform(housing_cat_encoded.reshape(-1, 1)).toarray()
+    housing_wihout_gaps["ocean_proximity"] = housing_cat_1hot
+    return housing_wihout_gaps
+
+
 
 def datawork(data):
     # s - dlugosc kolka, # c - co oznaczyc kolorami
@@ -66,11 +121,14 @@ def datawork(data):
         c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True,
     )
     # wyliczanie macierzy korelacji, dla kazdej mozliwej pary
-    corr_matrix = data.corr()
+    #corr_matrix = data.corr()
     # stopien korelacji kazdego atrybutu z mediana cen mieszkan
-    print(corr_matrix["median_house_value"].sort_values(ascending=False))
+    #print(corr_matrix["median_house_value"].sort_values(ascending=False))
     plt.legend()
     plt.show()
+    #housing = load_train_data().drop("median_house_value", axis=1)
+    #housing_labels = load_train_data()["median_house_value"].copy()
+
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
@@ -79,7 +137,8 @@ if __name__ == '__main__':
         fetch_housing_data()
     elif sys.argv[1] == "show":
         data = load_housing_data()
-        print(data)
+        for k in data:
+            print(k)
         #print(data["ocean_proximity"].value_counts())
         #print(data.describe())
         #data.hist(bins=50, figsize=(20, 15))
